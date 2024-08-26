2b485fc5dc6cef0fabb2e2b89e0cb355afc54fef
Andrew <ailen1002@163.com>
2024-08-26 14:14:22 +0800

    增加model_account数据模型

---
+++ Models/model_account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace password.Models;
+
+public class ModelAccount
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string? AccountName { get; set; }
+
+    [Required]
+    public string? Account { get; set; }
+
+    [Required]
+    public string? Password { get; set; }
+
+    [Required]
+    [DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+    public string CreationDate { get; set; } = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+}

fae26e020cfee97142db492f407ad3a5f5dbdfb9
Andrew <ailen1002@163.com>
2024-09-05 10:42:40 +0800

    创建User的Model

--- Models/Users.cs
+++ Models/Users.cs
@@ -1,6 +1,13 @@
-﻿namespace password.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace password.Models;
 
 public class Users
 {
-    
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string UserName { get; set; }
+    [Required]
+    public string PasswordHash  { get; set; }
 }

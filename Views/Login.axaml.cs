ffad2b3a1c697556d370cd53f1e2d786c4f2ed36
Andrew <ailen1002@163.com>
2024-09-27 08:34:48 +0800

    增加登录页面回车调用LoginCommand功能

--- Views/Login.axaml.cs
+++ Views/Login.axaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Input;
 using Avalonia.Markup.Xaml;
 using password.ViewModels;
 
@@ -11,5 +12,13 @@ public partial class Login : Window
     public Login()
     {
         InitializeComponent();
+    }
+    // 监听键盘事件
+    private void PasswordBox_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            (DataContext as LoginViewModel)?.LoginCommand.Execute().Subscribe();
+        }
     }
 }
